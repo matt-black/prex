fix variational approx of gmm kl

src/prex/gmm/dist.py
@@ -687,33 +687,42 @@ def kullback_leibler_gmm_approx_var_spherical(
     wgt_q: Float[Array, " m"],
     var_p: float,
     var_q: float,
-    n_dim: int,
 ) -> Float[Array, ""]:
-    kl_fun = Partial(
-        kullback_leibler_gaussian_spherical,
-        var_p=var_p,
-        var_q=var_q,
-        n_dim=n_dim,
-    )
 
-    def kl_combos(
-        mu_a: Float[Array, "n d"],
-        mu_b: Float[Array, "m d"],
-        wgt_b: Float[Array, " m"],
-    ):
-        return jax.vmap(
-            lambda a: jax.vmap(
-                lambda b: jnp.exp(jnp.negative(kl_fun(a, b))), 0, 0
-            )(mu_b)
-            * wgt_b,
+    self_e = jax.vmap(
+        lambda a: jax.vmap(
+            lambda b, w: w
+            * jnp.exp(
+                jnp.negative(
+                    kullback_leibler_gaussian_spherical(a, b, var_p, var_p, 3)
+                )
+            ),
+            (0, 0),
             0,
-            0,
-        )(mu_a)
+        )(mu_p, wgt_p),
+        0,
+        0,
+    )(mu_p)
 
-    self_e = kl_combos(mu_p, mu_p, wgt_p)
+    cross_e = jax.vmap(
+        lambda a: jax.vmap(
+            lambda b, w: w
+            * jnp.exp(
+                jnp.negative(
+                    kullback_leibler_gaussian_spherical(a, b, var_p, var_q, 3)
+                )
+            ),
+            (0, 0),
+            0,
+        )(mu_q, wgt_q),
+        0,
+        0,
+    )(mu_p)
+
     top = jnp.sum(self_e, axis=1)
-    bot = jnp.sum(kl_combos(mu_p, mu_q, wgt_q), axis=1)
-    return jnp.sum(jnp.multiply(wgt_p, jnp.divide(top, bot)))
+    bot = jnp.sum(cross_e, axis=1)
+    val = jnp.sum(jnp.multiply(wgt_p, jnp.divide(top, bot)))
+    return val
 
 
 def gaussian_entropy(cov: Float[Array, "d d"]) -> Float[Array, ""]:

src/prex/gmm/opt.py
@@ -39,6 +39,12 @@ class Regularization(Enum):
     RIDGE = 2
 
 
+class Optimizer(Enum):
+    LBFGS = 0
+    ADAM = 1
+    SGD = 2
+
+
 type AuxiliaryData = tuple[
     Float[Array, ""], Float[Array, ""], Float[Array, " p"]
 ]
@@ -236,6 +242,8 @@ def _make_transform_function_spherical(
                 p: Float[Array, " p"],
             ) -> Float[Array, "m d"]:
                 A, t, wgt = tps.unpack_params_3d(p)
+                A = jax.lax.stop_gradient(A)
+                t = jax.lax.stop_gradient(t)
                 return tps.transform_basis(B, A, t, wgt)
 
         else:
@@ -298,8 +306,8 @@ def _make_loss_function_spherical(
                     wgts_mov,
                     var_ref,
                     var_mov,
-                    n_dim,
                 )
+                jax.debug.print("{}", dist)
                 return dist, (dist, jnp.array(0.0), p)
 
         elif metric == DistanceFunction.L2:
@@ -410,7 +418,6 @@ def _make_loss_function_spherical(
                         wgts_mov,
                         var_ref,
                         var_mov,
-                        n_dim,
                     )
                     e_bend = calculate_bending_energy(wgts)
                     return dist + reg_const * e_bend, (dist, e_bend, p)
@@ -477,7 +484,6 @@ def _make_loss_function_spherical(
                         wgts_mov,
                         var_ref,
                         var_mov,
-                        n_dim,
                     )
                     ridge_penalty = jnp.linalg.norm(wgts)
                     return dist + reg_const * ridge_penalty, (
@@ -543,7 +549,8 @@ def spherical(
     ctrl_pts: Float[Array, "c d"] | None = None,
     save_path: str | None = None,
     valid_pts: tuple[Float[Array, "v d"], Float[Array, " v"]] | None = None,
-    **lbfgs_kwargs,
+    optax_opt: Optimizer = Optimizer.LBFGS,
+    **optim_kwargs,
 ) -> tuple[Float[Array, " p"], tuple[Float[Array, ""], Float[Array, ""], int]]:
     loss_func = _make_loss_function_spherical(
         means_ref,
@@ -593,7 +600,14 @@ def spherical(
         loss_val, _ = loss_func(pars)
         return loss_val
 
-    optimizer = optax.lbfgs(**lbfgs_kwargs)
+    if optax_opt == Optimizer.LBFGS:
+        optimizer = optax.lbfgs(**optim_kwargs)
+    elif optax_opt == Optimizer.ADAM:
+        optimizer = optax.adam(**optim_kwargs)
+    elif optax_opt == Optimizer.SGD:
+        optimizer = optax.sgd(**optim_kwargs)
+    else:
+        raise ValueError("invalid optimizer")
     opt_state = optimizer.init(init_pars)
 
     def keep_stepping(x: OptimizationState) -> Bool:

src/prex/gmm/tps.py
@@ -198,7 +198,7 @@ def transform_basis(
     rbf_wgts: Float[Array, "n_ctrl-d d"],
 ) -> Float[Array, "n_comp d"]:
     par = jnp.concatenate(
-        [affine, translation[jnp.newaxis, :], rbf_wgts], axis=0
+        [translation[jnp.newaxis, :], affine, rbf_wgts], axis=0
     )
     return basis @ par
 
@@ -255,12 +255,13 @@ def initialize_params(
     n_dim: int,
     init_aff: Float[Array, "{n_dim} {n_dim}"] | None,
     init_trans: Float[Array, " {n_dim}"] | None,
+    epsilon: float = 1e-6,
 ) -> Float[Array, " p"]:
     if init_aff is None:
         init_aff = jnp.eye(n_dim)
     if init_trans is None:
         init_trans = jnp.zeros((n_dim,))
-    init_wgt = jnp.zeros((n_ctrl_pts - n_dim - 1, n_dim))
+    init_wgt = jnp.ones((n_ctrl_pts - n_dim - 1, n_dim)) * epsilon
     return jnp.concatenate(
         [init_aff.ravel(), init_trans.ravel(), init_wgt.ravel()], axis=0
     )